bae2bb7afd162adf281190a2725a213f1f227aa2
Paritosh Sabade <77101080+ParitoshSabade@users.noreply.github.com>
2023-10-03 00:06:46 -0400

    Add files via upload

---
+++ BackgroundThreadDemo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace TrainingProject
+{
+	public class BackgroundThreadDemo
+	{
+        private static readonly Dictionary<string, int> keyValueStore = new Dictionary<string, int>();
+        
+
+        static void Main()
+        {
+            
+            Thread backgroundThread = new Thread(BackgroundWork);
+            backgroundThread.IsBackground = true;
+            backgroundThread.Start();
+
+            // Sleep for 2 seconds to allow the background thread to set values
+            Thread.Sleep(2000);
+
+            
+            
+            foreach (var kvp in keyValueStore)
+            {
+                Console.WriteLine($"Key: {kvp.Key}, Value: {kvp.Value}");
+            }
+            
+        }
+
+        static void BackgroundWork()
+        {
+            
+            
+            keyValueStore["Key1"] = 10;
+            keyValueStore["Key2"] = 29999;
+            keyValueStore["Key3"] = 9876;
+            
+        }
+    }
+}
+
---
+++ ThreadPoolDemo.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+class ThreadPoolDemo
+{
+    private static readonly Dictionary<string, int> keyValueStore = new Dictionary<string, int>();
+    private static readonly object lockObject = new object();
+
+    static void Main()
+    {
+        ThreadPool.QueueUserWorkItem(KeyValueWorker, new KeyValueAction("set", "key1", 10));
+        Thread.Sleep(1000);
+        ThreadPool.QueueUserWorkItem(KeyValueWorker, new KeyValueAction("get", "key1"));
+        ThreadPool.QueueUserWorkItem(KeyValueWorker, new KeyValueAction("set", "key2", 100));
+        Thread.Sleep(1000);
+        ThreadPool.QueueUserWorkItem(KeyValueWorker, new KeyValueAction("get", "key2"));
+        ThreadPool.QueueUserWorkItem(KeyValueWorker, new KeyValueAction("get", "key2"));
+        ThreadPool.QueueUserWorkItem(KeyValueWorker, new KeyValueAction("set", "key2", 2000));
+        ThreadPool.QueueUserWorkItem(KeyValueWorker, new KeyValueAction("get", "key2"));
+        ThreadPool.QueueUserWorkItem(KeyValueWorker, new KeyValueAction("get", "key2"));
+
+        // Sleep to allow time for the thread pool tasks to complete
+        Thread.Sleep(2000);
+
+
+    }
+
+    static void KeyValueWorker(object state)
+    {
+        KeyValueAction action = (KeyValueAction)state;
+        if (action.Action == "set")
+        {
+            string result = KeyValueDatabase("set", action.Key, action.Value);
+            Console.WriteLine(result);
+        }
+        else if (action.Action == "get")
+        {
+            string result = KeyValueDatabase("get", action.Key);
+            Console.WriteLine($"Result of 'get {action.Key}': {result}");
+        }
+    }
+
+    static string KeyValueDatabase(string action, string key, int value = 0)
+    {
+        lock (lockObject)
+        {
+            if (action == "set")
+            {
+                keyValueStore[key] = value;
+                return $"Value({key},{value}) set successfully.";
+            }
+            else if (action == "get")
+            {
+                if (keyValueStore.ContainsKey(key))
+                {
+                    return keyValueStore[key].ToString();
+                }
+                else
+                {
+                    return "Key not found.";
+                }
+            }
+            else
+            {
+                return "Invalid action.";
+            }
+        }
+    }
+}
+
+class KeyValueAction
+{
+    public string Action { get; }
+    public string Key { get; }
+    public int Value { get; }
+
+    public KeyValueAction(string action, string key, int value = 0)
+    {
+        Action = action;
+        Key = key;
+        Value = value;
+    }
+}
